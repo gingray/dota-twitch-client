5cddc60bf8c3c32dbf8dd1cdea0912b3832fa25e
gingray <gingray.dev@gmail.com>
2022-12-03 14:15:16 +0100

    improve UI

--- src/components/HeroPlayer.tsx
+++ src/components/HeroPlayer.tsx
@@ -1,9 +1,24 @@
+import classNames from "classnames";
+import {OverlayTrigger, Tooltip} from "react-bootstrap";
 export const HeroPlayer = ({heroPlayer}: {heroPlayer:any}) => {
-
     const heroImage = heroPlayer.Hero.HeroName.toLowerCase().replaceAll(" ", "_") + '_vert.png'
+    const renderTooltip = (props:any) => (
+        <Tooltip id="button-tooltip" {...props}>{heroPlayer.PlayerName}</Tooltip>
+    );
+    const css = classNames({
+        "hero-player-card": true,
+        "tracked-player-card": heroPlayer.IsTrackedPlayer
+    })
+    if (heroPlayer.IsTrackedPlayer) {
+        console.log(heroPlayer)
+    }
     return (
-        <>
+        <OverlayTrigger  placement="top"
+                         delay={{ show: 150, hide: 400 }}
+                         overlay={renderTooltip}>
+        <span className={css}>
             <img alt={heroPlayer.Hero.HeroName} width={42} height={60} src={process.env.PUBLIC_URL + `/images/heroes/${heroImage}`}/>
-        </>
+        </span>
+        </OverlayTrigger>
     )
 }
--- src/components/MatchSide.tsx
+++ src/components/MatchSide.tsx
@@ -2,7 +2,7 @@ import {HeroPlayer} from "./HeroPlayer";
 
 export const MatchSide = ({players, isVictory}:{players:any, isVictory: boolean}) => {
     const cssClass = isVictory ? 'victory' : 'defeat'
-    console.log('players', players)
+    // console.log('players', players)
     const heroPlayers = players.map((item:any) => <HeroPlayer key={item.ID.toString()} heroPlayer={item}/>)
     return (
         <div className={`match-side ${cssClass}`}>
